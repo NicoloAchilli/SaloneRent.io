6dc383b72c3a1d457dead723b6e5ecc00903d18d
NicoloAchilli <s1098682@studenti.univpm.it>
2023-10-26 11:09:26 +0200

    aggiunta model,tabella e seeder delle faq

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Resources\Faq;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -19,6 +20,7 @@ class DatabaseSeeder extends Seeder
             StaffSeeder::class,
             AdminSeeder::class,
             CarSeeder::class,
+            FaqSeeder::class,
         ]);
     }
 }
